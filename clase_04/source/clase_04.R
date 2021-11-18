7a25347796610561fb56f7e93f09b29c08c3c911
eduard-martinez <edfemagonza@gmail.com>
2021-11-17 22:56:42 -0500

    update clase_04

--- clase_04/source/clase_04.R
+++ clase_04/source/clase_04.R
@@ -8,15 +8,15 @@ Sys.setlocale("LC_CTYPE", "en_US.UTF-8") # Encoding UTF-8
 
 # install/load packages
 require(pacman)
-p_load(tidyverse,rio,skimr,png,grid,data.table,viridis,ggthemes)
+p_load(tidyverse,rio,skimr,data.table,viridis,ggthemes,png,grid)
 
 ## Hoy veremos
 
 ### ***1.*** Aplicación GEIH: Importar, exportar 
 
-### ***2.*** group_by() y summarize()♥
+### ***2.*** group_by() y summarize()
 
-### ***3.*** ggplot()
+### ***3.*** Visualizaciones
 
 
 #=====================#
@@ -24,19 +24,16 @@ p_load(tidyverse,rio,skimr,png,grid,data.table,viridis,ggthemes)
 #=====================#
 
 ## importar los archivos 
-
-cat("Vectores eje de las rutas de los archivos")
+cat("Vectores con las rutas de los archivos")
 
 jun_2019 = list.files("clase_04/input/2019/Junio.csv",full.names=T)
 jun_2020 = list.files("clase_04/input/2020/Junio.csv",full.names=T)
 files = c(jun_2019,jun_2020)
 
-cat("Filtramos para quedarnos con solo datos de areas metropolitanas")
-
+# función grep()
 grep("rea -",files)
 files[grep("rea -",files)]
 files = files[grep("rea -",files)]
-
 
 #-------------------#
 ## importar datos
@@ -56,115 +53,56 @@ inac =  import_list(file = files[grep("Inac",files)])
 #### Importamos la base de fuerza de trabajo
 ft = import_list(file = files[grep("Fuerza",files)])
 
-
-#---------------------------#
-## agregamos variables
-cat("Agregamos la variable años y una variable identificadora")
-
-#### Caracteristicas Generales
-cg[[1]]$year=2019
-cg[[2]]$year=2020
-
-#### Ocupados
-ocu[[1]]$ocu=1
-ocu[[2]]$ocu=1
-
-#### Desocupados
-deso[[1]]$deso=1
-deso[[2]]$deso=2
-
-#### Inactivos 
-inac[[1]]$inac=1
-inac[[2]]$inac=1
-
-#### Fuerza de Trabajo
-ft[[1]]$fuerza=1
-ft[[2]]$fuerza=1
-
-#--------------------------#
-## Variables en minusculas
-cat("se convierte los nombres a minusculas")
-
-#### Caracteristicas Generales
-colnames(cg[[1]]) = tolower(colnames(cg[[1]]))
-colnames(cg[[2]]) = tolower(colnames(cg[[2]]))
-
-#### Ocupados
-colnames(ocu[[1]]) = tolower(colnames(ocu[[1]]))
-colnames(ocu[[2]]) = tolower(colnames(ocu[[2]]))
-
-#### Desocupados 
-colnames(deso[[1]]) = tolower(colnames(deso[[1]]))
-colnames(deso[[2]]) = tolower(colnames(deso[[2]]))
-
-#### Inactivos
-colnames(inac[[1]]) =tolower(colnames(inac[[1]]))
-colnames(inac[[2]]) = tolower(colnames(inac[[2]]))
-
-#### Fuerza de Trabajo 
-colnames(ft[[1]])=tolower(colnames(ft[[1]]))
-colnames(ft[[2]])=tolower(colnames(ft[[2]]))
-
+#-----------------------#
+## limpiar variables
+source("clase_04/source/clean_data.R") # ejecutar un código auxiliar
 
 #-----------------------#
-## rbindlist 
-cat("rbind permite pegar las listas por medio de sus nombres")
+## función rbindlist()
+cat("rbind permite unir los elementos de una listas (agregar observaciones)")
 
-cg_ready<- rbindlist(l=cg, use.names = TRUE, fill = TRUE)
+cg_bind = rbindlist(l=cg, use.names = TRUE, fill = TRUE)
 
-ocu_ready <- rbindlist(l=ocu,use.names = TRUE,fill = TRUE)
+ocu_bind = rbindlist(l=ocu,use.names = TRUE,fill = TRUE)
 
-deso_ready <- rbindlist(l=deso,use.names = TRUE,fill = TRUE)
+deso_bind = rbindlist(l=deso,use.names = TRUE,fill = TRUE)
 
-inac_ready = rbindlist(l=inac,use.names = TRUE,fill = TRUE)
+inac_bind = rbindlist(l=inac,use.names = TRUE,fill = TRUE)
 
-ft_ready = rbindlist(l=ft,use.names = TRUE,fill=TRUE)
-
-
-#--------------------------#
-## Simpificacion
-
-cat("para simplificar importaremos selecionaremos variables en las cuales hay un interes.")
-
-cg_ready = cg_ready %>% select("secuencia_p", "directorio","orden","esc","dpto","year","fex_c_2011","p6020")
-
-ocu_ready = ocu_ready %>% select("secuencia_p", "directorio","orden","ocu")
-
-deso_ready = deso_ready %>% select("secuencia_p", "directorio","orden","deso")
-
-inac_ready = inac_ready %>% select("secuencia_p" ,"directorio","orden","inac")
-
-ft_ready = ft_ready %>% select("secuencia_p", "directorio","orden","fuerza")
-
-
+ft_bind = rbindlist(l=ft,use.names = TRUE,fill=TRUE)
+ 
 #--------------------------#
 #### gaih completa
-cat("left_join, culmina este trabajo permitiendo pegar horizontalmente")
+cat("unir bases de datos (agregar variables)")
 
-geih = left_join(x = cg_ready, y = ft_ready, by = c("secuencia_p","directorio","orden")) %>% 
-  left_join(x = ., y = inac_ready, by = c("secuencia_p","directorio","orden")) %>% 
-  left_join(x = ., y = ocu_ready, by = c("secuencia_p","directorio","orden")) %>% 
-  left_join(x = ., y = deso_ready, by = c("secuencia_p","directorio","orden")) %>% as_tibble()
+geih = left_join(x = cg_bind, y = ft_bind, by = c("secuencia_p","directorio","orden")) %>% 
+       left_join(x = ., y = inac_bind, by = c("secuencia_p","directorio","orden")) %>% 
+       left_join(x = ., y = ocu_bind, by = c("secuencia_p","directorio","orden")) %>% 
+       left_join(x = ., y = deso_bind, by = c("secuencia_p","directorio","orden")) %>% as_tibble()
 
-# limpiemos parte del entorno
-rm(cg,cg_ready,deso,deso_ready,ft,ft_ready,inac,inac_ready,ocu,ocu_ready,files,jun_2019,jun_2020)
+# limpiar entorno
+grep("geih",ls())
+ls()[grep("geih",ls())]
+rm(list=ls()[-grep("geih",ls())])
 
+#=============================#
+# 2. Collapsar bases de datos #
+#=============================#
 
-#==========================#
-# 2. limpiar base de datos #
-#==========================#
+# inspeccionar conjunto de datos
+skim(geih)
+
 # limpiar el factor de expansion
 geih$fex_c_2011 = gsub(",","\\.",geih$fex_c_2011) %>% as.numeric()
 
 geih = geih %>%
-  mutate(inac = ifelse(is.na(inac),0,1),
-         ocu = ifelse(is.na(ocu),0,1),
-         deso = ifelse(is.na(deso),0,1),
-         fuerza = ifelse(is.na(fuerza),0,1))
+       mutate(inac = ifelse(is.na(inac),0,1),
+              ocu = ifelse(is.na(ocu),0,1),
+              deso = ifelse(is.na(deso),0,1),
+              fuerza = ifelse(is.na(fuerza),0,1))
 
 #-------------------------#
 ## summarise 
-cat("crea una nueva data.frame en el cual se encuenta los resultados de diferentes operaciones")
 
 #### Media
 mean(geih$p6500, na.rm = T)
@@ -173,7 +111,8 @@ geih %>% summarise(media = mean(p6500, na.rm = TRUE))
 #### Mediana
 median(geih$p6500, na.rm = TRUE)
 geih %>% summarise(media = mean(p6500, na.rm = T) ,
-                      mediana = median(p6500, na.rm = T)) 
+                   mediana = median(p6500, na.rm = T)) 
+
 #### frecuencia
 table(geih$p6020) 
 geih %>% summarise(total = table(p6020)) 
@@ -181,7 +120,6 @@ geih %>% summarise(total = table(p6020))
 #### Quartiles
 quantile(geih$p6500, na.rm = TRUE)
 geih %>% summarise(quartiles = quantile(p6500, na.rm = TRUE)) 
-
 
 
 #-------------------------#
@@ -198,10 +136,11 @@ t_des = desocupados %>% left_join(., p_activa, by = c("p6020", "year")) %>%
                                unemployment = total_desempleados/total_activa*100)
 
 
-#========#
-# ggplot #
-#========#
-cat("ggplot funciona por medio de capas cada + es una capa diferente")
+#====================#
+# 3. Visualizaciones #
+#====================#
+
+cat("ggplot funciona por medio de capas y se usa el signo + para adiccionar un atributo al gráfico")
 ggplot(t_des) 
 
 #### pintamos sobre la primera capa
@@ -209,16 +148,15 @@ ggplot() + geom_bar(data = t_des)
 
 #### pintamos sobre la segunda capa
 ggplot(data = t_des, aes(x = unemployment, y = as.factor(year))) + 
-       geom_bar(position="dodge", stat="identity") 
+geom_bar(position="dodge", stat="identity") 
 
 #### para mantener cambiar los ejes
 ggplot(data = t_des, aes(x = unemployment, y = as.factor(year))) + 
-  geom_bar(position="dodge", stat="identity") + coord_flip()
+geom_bar(position="dodge", stat="identity") + coord_flip()
 
 #### podemos incluir colores
 ggplot(data = t_des, aes(x = unemployment, y = as.factor(year), fill = as.factor(p6020))) + 
-  geom_bar(position="dodge", stat="identity") + coord_flip()
-
+geom_bar(position="dodge", stat="identity") + coord_flip()
 
 
 #================#
