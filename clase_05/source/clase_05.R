d0106be171c1a74d8b2ef7472198c1f67ca2758e
eduard-martinez <edfemagonza@gmail.com>
2021-11-25 10:02:43 -0500

    update clase_05

--- clase_05/source/clase_05.R
+++ clase_05/source/clase_05.R
@@ -11,7 +11,7 @@ set.seed(12345) # fijar semilla
 
 # load packages
 require(pacman)
-p_load(tidyverse, rio, data.table, png, grid)
+p_load(tidyverse , rio , data.table , png , grid)
 
 ## Hoy veremos
 
@@ -29,14 +29,30 @@ p_load(tidyverse, rio, data.table, png, grid)
 ## Estructura de datos
 dev.off()
 grid.raster(readPNG("pics/bucle_flow.png")) # source: datacamp
+browseURL("")
 
 #----------------------#
 ## Estructura de un bucle (for)
 vector =  1:5
-
-for (i in vector){ # Vector sobre el que se va a aplicar el loop
+for (i in c(1,2,3,4,5)){ # Vector sobre el que se va a aplicar el loop
      i = i*i  # Sobreescribe i como el resultado de i*i
      print(i) # Pinta el resultado sobre la consola
+}
+
+results = tibble(i = 1:5 , resultado = NA)
+for (i in vector){ # Vector sobre el que se va a aplicar el loop
+     n = i
+     i = i*i  # Sobreescribe i como el resultado de i*i
+     print(i) # Pinta el resultado sobre la consola
+     results$resultado[n] = i
+}
+
+results1 = tibble(i = 1:5 , resultado = NA)
+for (i in vector){ # Vector sobre el que se va a aplicar el loop
+     n = i
+     i = i*i*i  # Sobreescribe i como el resultado de i*i
+     print(i) # Pinta el resultado sobre la consola
+     results1$resultado[n] = i
 }
 
 #----------------------#
@@ -54,14 +70,14 @@ repeat{
 j = 1
 while (j <= 5) { # condición
        print(j*j) # pintar sobre la consola el producto de j*j
-       j = j+1 # sobreescribir j como j + 1
+       #j = j+1 # sobreescribir j como j + 1
 }
 
 #----------------------#
 ## Veamos un ejemplo
 
 #### generate data
-df = data.frame(cod_mpio = c(5001,5002,5003,5004,5005,5006),	
+df = tibble(cod_mpio = c(5001,5002,5003,5004,5005,5006),	
                 violencia_2014	= c(0.05,0.07,0.06,0.03,0.04,0.03),
                 violencia_2015	= c(0.09,0.05,0.03,0.06,0.03,0.01),
                 violencia_2016	= c(0.02,0.04,0.03,0.02,0.03,0.00),
@@ -69,26 +85,26 @@ df = data.frame(cod_mpio = c(5001,5002,5003,5004,5005,5006),
                 violencia_2018	= c(0.01,0.02,0.04,0.05,0.07,0.01),
                 violencia_2019  = c(0.01,0.02,0.02,0.03,0.03,0.01))
 
+cat("El loop se aplica desde la columna 2 (porque la columna 1 contiene el código DANE) hasta la última columna del dataframe.")
 
-cat("El loop se aplica desde la columna 2 (porque la columna 1 contiene el código DANE) 
-    hasta la última columna del dataframe.")
-
+df
 for(i in 2:ncol(df)){
-    df[,i] <- df[,i]*100
+    df[,i] = df[,i]*100
 }
 df
 
 #----------------------#
 ## usando controles de flujo: if & else
-
 cat("los controles de flujo regulan la ejecucion de los codigos dentro de los diferentes tipos de loops")
-
+letters
 for (letra in letters) {
+  
      if (letra %in% c("a","e","i","o","u")){
-         print(paste(letra," - Es una vocal"))
+         print(paste0(letra," - Es una vocal"))
      }
+  
      else {
-           print(paste(letra," - NO es una vocal")) 
+           print(paste0(letra," - NO es una vocal")) 
      } 
 }
 
@@ -98,7 +114,7 @@ for (letra in letters) {
 cat("next es un condicional que le permite al loop saltar al siguiente elemento del loop")
 
 for(i in 1:20) {
-    if(i %% 2 == 0) next # se salta a la siguiente linea
+    if(i %% 2 == 0){ next } # se salta a la siguiente linea
     print(i)
 }
 
@@ -121,35 +137,31 @@ grid.raster(readPNG("pics/function_machine.png")) # source: https://fhernanb.git
 cat("message, warnings & errors indican diferentes niveles de mensajes. 
      *message: informa resultados... etc. 
      *warnings: informa algun tipo de error o anuncio importante que no impide el funcionamiento del programa.
-     *error: indicador sobre el funcionamiento correcto de la funcion.")
+     *error: indicador sobre el fua = ncionamiento correcto de la funcion.")
   
 unir = function(x, y){
-       palabra = paste(x ,y) %>% toupper()
-       message(paste("las combinacion de las palabras producen:", palabra))
+       palabra = paste0(x," - ",y) %>% toupper()
+       message(paste("message: las combinacion de las palabras producen:", palabra))
 }
 unir(x = "hola", y = "clase")
 
 #----------------------#
 ## Veamos un ejemplo (...)
-remove_na = function(x) x = ifelse(is.na(x),0,x)
+remove_na = function(x) x = ifelse(is.na(x)==T,0,x)
 
 vector = c(1:5,rep(NA,5),11:15)
 
 vector
-vector = remove_na(vector)
+vector = remove_na(x = vector)
 vector
 
 storms %>% head()
-df = storms %>% as_tibble() %>% mutate(ts_diameter=remove_na(ts_diameter))
+df = storms %>% as_tibble() 
+df$ts_diameter
+remove_na(x = df$ts_diameter) %>% print()
+df = df %>% mutate(ts_diameter = remove_na(ts_diameter),
+                   hu_diameter = ifelse(is.na(hu_diameter)==T,0,hu_diameter))
 df %>% head()
-
-#----------------------#
-## Veamos un ejemplo (...)
-join_words = function(x, y){
-             palabra = paste(x ,y) %>% toupper()
-             return(palabra)
-}
-join_words(x = "hola", y = "mundo")
 
 #----------------------#
 ## Veamos un ejemplo (...)
@@ -193,18 +205,26 @@ dev.off()
 grid.raster(readPNG("pics/apply.png"))
 
 ####  Operaciones por columnas
-apply(X = mtcars, MARGIN = 2, function(x) min(x)) 
+mtcars
+apply(X = mtcars, MARGIN = 2, FUN = min)
+apply(mtcars , 2 , function(columna) min(...=columna , na.rm=T)) 
 
 ####  Operaciones por filas
-apply(X = mtcars, MARGIN = 1, function(x)  print(x[8:11]))
+apply(X = mtcars, MARGIN = 1, function(x) sum(x))
 
 #----------------------#
 ## Lapply
 dev.off()
 grid.raster(readPNG("pics/lapply.png"))
 
+lapply(mtcars, function(x) summary(x))
 lap = lapply(mtcars, function(x) summary(x))
 lap
+
+storms
+table(is.na(storms$hu_diameter))
+table(is.na(storms$ts_diameter))
+lapply(storms ,function(x)  table(is.na(x)))
 
 #----------------------#
 ## Sapply
@@ -228,8 +248,29 @@ ejemplo
 
 #----------------------#
 ## Importar datos
-list_chip = import_list(file = list.files("clase_05/input/chip",full.names=T, recursive = TRUE) %>% unlist())
-names(list_chip) 
+list.files("clase_05/input/chip",full.names=T, recursive = T) 
+path_chip = list.files("clase_05/input/chip",full.names=T, recursive = TRUE) %>% unlist()
+
+# librería rio
+list_chip = import_list(file = path_chip)
+
+lista_lapply = lapply(path_chip, function(path) import(file = path) %>% mutate(ruta = path))
+
+f_read = function(x) import(file = x) %>% mutate(ruta = x)
+lista_f_read = lapply(path_chip, function(path) f_read(x = path))
+
+
+#----------------------#
+## Caso particular
+df = tibble(valor=NA,cod_dane=NA,periodo=NA)  
+lista_n = list_chip[[1]] 
+
+df$cod_dane = colnames(lista_n)[1]
+df$periodo = lista_n[2,1]
+
+# extraer el valor
+colnames(lista_n) = lista_n[7,]
+df$valor = lista_n %>% subset(NOMBRE=="EDUCACIÓN") %>% select(`PAGOS(Pesos)`) %>% as.numeric()
 
 #----------------------#
 ## Funcion para extraer
@@ -237,7 +278,7 @@ list_chip
 f_extrac = function(n,lista,tipo_rubro){
   
            # crear df
-           df = data.frame(valor=NA,cod_dane=NA,periodo=NA)  
+           df = tibble(valor=NA,cod_dane=NA,periodo=NA)  
            lista_n = lista[[n]] 
           
            # extraer codigo dane
@@ -248,12 +289,18 @@ f_extrac = function(n,lista,tipo_rubro){
           
            # extraer el valor
            colnames(lista_n) = lista_n[7,]
-           df$valor = lista_n %>% subset(NOMBRE==tipo_rubro) %>% select(`PAGOS(Pesos)`)
-          
+           valor = lista_n %>% subset(NOMBRE==tipo_rubro) %>% select(`PAGOS(Pesos)`) %>% gsub(",","",.) %>% as.numeric()
+           df$valor = valor
+             
+             
 return(df)  
 }
 f_extrac(n = 10 , lista = list_chip , tipo_rubro = "SALUD")
 
+datos_salud = lapply(1:length(list_chip) , function(x) f_extrac(n = x , lista = list_chip , tipo_rubro = "SALUD"))
+
+datos_salud = rbindlist(l = datos_salud , use.names = T , fill = T) %>% mutate(sector = "SALUD")
+  
 #======================#
 #=== Ejemplo: GEIH ====#
 #======================#
