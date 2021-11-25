fda12b8c97e9c1f566d3cbbd68ee93de7554f9cd
eduard-martinez <edfemagonza@gmail.com>
2021-11-25 07:57:02 -0500

    update clase_05

--- clase_05/source/clase_05.R
+++ clase_05/source/clase_05.R
@@ -7,6 +7,7 @@
 # initial configuration
 rm(list=ls()) # limpiar entorno
 Sys.setlocale("LC_CTYPE", "en_US.UTF-8") # Encoding UTF-8
+set.seed(12345) # fijar semilla
 
 # load packages
 require(pacman)
@@ -20,47 +21,40 @@ p_load(tidyverse, rio, data.table, png, grid)
 
 ### **3.** Familia apply
 
-
 #======================#
 #======= Bucles =======#         
 #======================#
-browseURL(url = "https://intro2r.com/loops.html#for-loop", browser = getOption("browser")) # loops
-browseURL(url = "https://mauricioanderson.com/curso-r-estructuras-control/", browser = getOption("browser")) # controles de flujo
 
 #----------------------#
 ## Estructura de datos
 dev.off()
-grid.raster(readPNG("pics/bucle_flow.png")) 
-
-# source
-browseURL(url = "http://community.datacamp.com.s3.amazonaws.com/community/production/ckeditor_assets/pictures/132/content_flowchart1.png")
+grid.raster(readPNG("pics/bucle_flow.png")) # source: datacamp
 
 #----------------------#
 ## Estructura de un bucle (for)
 vector =  1:5
 
 for (i in vector){ # Vector sobre el que se va a aplicar el loop
-  i = i*i  # Sobreescribe i como el resultado de i*i
-  print(i) # Pinta el resultado sobre la consola
+     i = i*i  # Sobreescribe i como el resultado de i*i
+     print(i) # Pinta el resultado sobre la consola
 }
 
 #----------------------#
 ## Estructura de un bucle (repeat)
 repeat{
-  m = rnorm(n=1 , mean=10 , sd=2) # generar un número aleatorio (media 10 , sd 2) 
-  print(m) # pintar el número sobre la consola
-  if (m <= 8){ # condicionar a que ese número sea menor o igual a 8
-    break # detener el loop si m es menor o igual a 8 
-  } 
+       m = rnorm(n=1 , mean=10 , sd=2) # generar un número aleatorio (media 10 , sd 2) 
+       print(m) # pintar el número sobre la consola
+       if (m <= 8){ # condicionar a que ese número sea menor o igual a 8
+           break # detener el loop si m es menor o igual a 8 
+       } 
 }
 
 #----------------------#
 ## Estructura de un bucle (while)
 j = 1
-
 while (j <= 5) { # condición
-  print(j*j) # pintar sobre la consola el producto de j*j
-  j = j+1 # sobreescribir j como j + 1
+       print(j*j) # pintar sobre la consola el producto de j*j
+       j = j+1 # sobreescribir j como j + 1
 }
 
 #----------------------#
@@ -74,39 +68,38 @@ df = data.frame(cod_mpio = c(5001,5002,5003,5004,5005,5006),
                 violencia_2017	= c(0.03,0.06,0.03,0.01,0.04,0.01),
                 violencia_2018	= c(0.01,0.02,0.04,0.05,0.07,0.01),
                 violencia_2019  = c(0.01,0.02,0.02,0.03,0.03,0.01))
-set.seed(12345)
+
 
 cat("El loop se aplica desde la columna 2 (porque la columna 1 contiene el código DANE) 
     hasta la última columna del dataframe.")
 
 for(i in 2:ncol(df)){
-  df[,i] <- df[,i]*100
+    df[,i] <- df[,i]*100
 }
-
 df
 
 #----------------------#
 ## usando controles de flujo: if & else
 
-cat("los controles de flujo controlan la ejecucion de los codigos dentro de los diferentes tipos de loops")
+cat("los controles de flujo regulan la ejecucion de los codigos dentro de los diferentes tipos de loops")
 
 for (letra in letters) {
-  if (letra %in% c("a","e","i","o","u")){
-    print(paste(letra," - Es una vocal"))
-  }
-  else {
-    print(paste(letra," - NO es una vocal")) 
-  } 
+     if (letra %in% c("a","e","i","o","u")){
+         print(paste(letra," - Es una vocal"))
+     }
+     else {
+           print(paste(letra," - NO es una vocal")) 
+     } 
 }
 
 
 #----------------------#
 ## Usando next
-cat("next es un condicional que indicar al llop que debe saltar a la siguiente linea")
+cat("next es un condicional que le permite al loop saltar al siguiente elemento del loop")
 
 for(i in 1:20) {
-  if(i %% 2 == 0) next # se salta a la siguiente linea
-  print(i)
+    if(i %% 2 == 0) next # se salta a la siguiente linea
+    print(i)
 }
 
 
@@ -119,40 +112,73 @@ browseURL(url = "https://www.r-bloggers.com/2016/02/functions-exercises/", brows
 browseURL(url = "https://adv-r.hadley.nz/conditions.html", browser = getOption("browser")) # errors, messages and warnings
 
 #----------------------#
-## Veamos un ejemplo (...)
+## Funciones
+dev.off()
+grid.raster(readPNG("pics/function_machine.png")) # source: https://fhernanb.github.io
 
+#----------------------#
+## Veamos un ejemplo (...)
 cat("message, warnings & errors indican diferentes niveles de mensajes. 
-      *message: informa resultados... etc. 
-      *warnings: informa algun tipo de error o anuncio importante que no impide el funcionamiento del programa.
-      *error: indicador sobre el funcionamiento correcto de la funcion.
-    ")
+     *message: informa resultados... etc. 
+     *warnings: informa algun tipo de error o anuncio importante que no impide el funcionamiento del programa.
+     *error: indicador sobre el funcionamiento correcto de la funcion.")
   
 unir = function(x, y){
-  palabra = paste(x ,y) %>% toupper()
-  
-  message(paste("las combinacion de las palabras producen:", palabra))
+       palabra = paste(x ,y) %>% toupper()
+       message(paste("las combinacion de las palabras producen:", palabra))
 }
 unir(x = "hola", y = "clase")
 
+#----------------------#
+## Veamos un ejemplo (...)
+remove_na = function(x) x = ifelse(is.na(x),0,x)
+
+vector = c(1:5,rep(NA,5),11:15)
+
+vector
+vector = remove_na(vector)
+vector
+
+storms %>% head()
+df = storms %>% as_tibble() %>% mutate(ts_diameter=remove_na(ts_diameter))
+df %>% head()
 
 #----------------------#
-## ejemplo con loop (...)
-vocales = function(frase) {
-          finaly = list()
-          
-          for (i in 1:nchar(frase)) { 
-            letra = substr(frase,i,i) %>% tolower()
-            
-            if(letra %in% c("a","e","i","o","u")){
-              finaly[i] = letra}
-            
-            if(letra %in% c(" ")){
-              warning("la frase contiene espacios vacios")}}
-          
-          finaly = finaly %>% unlist() %>% unique()
-          return(finaly)
+## Veamos un ejemplo (...)
+join_words = function(x, y){
+             palabra = paste(x ,y) %>% toupper()
+             return(palabra)
 }
-vocales(frase = "hola clase")
+join_words(x = "hola", y = "mundo")
+
+#----------------------#
+## Veamos un ejemplo (...)
+cat("funcion que regresa el producto de un numero por si mismo")
+num_2 = function(x){
+        c = x*x
+return(c)
+}
+num_2(x = 4)
+num_2(x = "A")
+
+
+# incluir controles de flujo
+num_2 = function(numero){
+  
+        # si es un numero
+        if (is.numeric(numero)){
+            c = numero*numero
+        return(c)
+        }
+        
+        # si no es un numero
+        if (is.numeric(numero)==F){
+            warning(paste0(numero," no es un número"))
+        }
+}
+num_2(numero = 10)
+num_2(numero = "hola")
+num_2(numero = "10")
 
 
 #======================#
@@ -190,41 +216,68 @@ grid.raster(readPNG("pics/sapply.png"))
 sap = sapply(mtcars, summary)
 sap
 
+#=========================#
+#==== Aplicación: chip ===#
+#=========================#
+rm(list=ls()) # limpiamos entorno
+
+#----------------------#
+## Chip
+browseURL("https://www.chip.gov.co/schip_rt/index.jsf")
+
+ejemplo = import("clase_05/input/chip/2019/11767600044K212410-1220191625694914330.xls", skip = 7)
+ejemplo
+
+#----------------------#
+## Importar datos
+list_chip = import_list(file = list.files("clase_05/input/chip",full.names=T, recursive = TRUE) %>% unlist())
+names(list_chip) 
+
+#----------------------#
+## Funcion para extraer datos
+f_extrac = function(lista,tipo_rubro){
+  
+           n = lapply(lista, function(y){ # lapply para repetir la funcion en cada elmento
+               fecha = y[2,1] # extraer la fecha
+               lugar = colnames(y)[1] # extraer el lugar
+                  
+               y = y[grep(paste(tipo_rubro, collapse="|"), y[,2]),c(2,4:8)] %>% 
+                   mutate(fecha = fecha, lugar = lugar) # extraer datos numericos
+                  
+              return(y)
+              }) %>% rbindlist(use.names = TRUE) 
+          
+          colnames(n) = c("nombre", "presupuesto_inicial","presupuesto_definitivo","compromisos","total_obligaciones","PAGOS", "fecha", "lugar") 
+          n 
+}
+
+resultados = f_extrac(list_chip, tipo_rubro = c("TOTAL INVERSIÓN","DEPORTE Y RECREACIÓN"))
 
 #======================#
 #=== Ejemplo: GEIH ====#
 #======================#
 rm(list=ls()) # limpiamos entorno
 
-## Ejemplo: importar la GEIH
-cat("se usa list. files para importar todos los archivos de la geih & 
-    recursive para indicar que entre dentro de todas las carpetas dentro de archivo")
-
 #### lista de los achivos 
-files_meses = list.files("clase_05/input/geih/",full.names = T, recursive = TRUE) %>% unlist()
+files_meses = list.files("clase_05/input/geih",full.names = T, recursive = TRUE) %>% unlist()
 
-#### funcion grep
-cat("grep encuentra todas la locacion dentro de un vector en el cual se encuetra el argumento que buscamos")
+#### recordando la funcion grep
 grep("Caract" ,files_meses)
-
-cat("se puede llamar un elemento dentro del vector por su numero")
 files_meses[c(1,9,17,25,30,35)]
 files_meses[grep("Caract" ,files_meses)]
 
 #----------------------#
 ## Funcion para extraer datos
-  
 f_read = function(x, patron){
-  
 lapply(x[grep(patron ,x)], function(y){
-                              year = substr(y,22,25)  # substraer los elementos en la posición 12-15 (YEAR)
-                              rural = substr(y,37,41) 
+                           year = substr(y,22,25)  # substraer los elementos en la posición 12-15 (YEAR)
+                           rural = substr(y,37,41) 
                               
-                              data = import(file = y) %>% rename_with(tolower) %>%  # se importa la base de datos
-                                                          mutate(year = year, # se anexa el year
-                                                                 rural = case_when(rural == "Resto" ~ 1, 
-                                                                                   rural != "Resto" ~ 0)) # se anexa si es rural
-  }) %>% rbindlist(use.names = TRUE, fill = TRUE) %>% as_tibble() # se juntan las bases de datos
+                           data = import(file = y) %>% 
+                                  rename_with(tolower) %>%  # se importa la base de datos
+                                  mutate(year = year, # se anexa el year
+                                  rural = case_when(rural == "Resto" ~ 1, rural != "Resto" ~ 0)) # se anexa si es rural
+}) %>% rbindlist(use.names = TRUE, fill = TRUE) %>% as_tibble() # se juntan las bases de datos
 }
 
 #----------------------#
@@ -244,54 +297,7 @@ geih = left_join(x = c_gen, y = ocu, by = c("secuencia_p","directorio","orden"))
 geih
 
 
-#======================#
-#==== Ejemplo: chip ===#
-#======================#
-rm(list=ls()) # limpiamos entorno
 
-#----------------------#
-## ship
-  
-cat(" CHIP contiene información financiera, relacionada con la contabilidad, tesorería y presupuesto de las entidades estatales; 
-    igualmente, los estados financieros de cada una de las entidades públicas que realizan su reporte al CHIP y los informes de control interno contable")
-
-ejemplo = import("clase_05/input/chip/2019/11767600044K212410-1220191625694914330.xls", skip = 7)
-ejemplo
-
-#----------------------#
-## Importar datos
-list_chip = import_list(file = list.files("clase_05/input/chip/",full.names=T, recursive = TRUE) %>% unlist())
-
-names(list_chip) 
-
-#----------------------#
-## grep & paste
-cat("paste con collapse, permite poner un | == (o) entre las palabras")
-paste(c("TOTAL INVERSIÓN","DEPORTE Y RECREACIÓN"), collapse="|")
-
-grep(paste(c("TOTAL INVERSIÓN","DEPORTE Y RECREACIÓN"), collapse="|"), ejemplo[,2]) #posición donde se encuentran las palabras
-
-ejemplo[grep(paste(c("TOTAL INVERSIÓN","DEPORTE Y RECREACIÓN"), collapse="|"), ejemplo[,2]),c(2,4:8)] # filas donde se encuentran las palabras
-
-#----------------------#
-## Funcion para extraer datos
-f_extrac = function(lista,tipo_rubro){
-  
-  n =lapply(lista, function(y){ # lapply para repetir la funcion en cada elmento
-    
-                              fecha = y[2,1] # extraer la fecha
-                              lugar = colnames(y)[1] # extraer el lugar
-    
-    y = y[grep(paste(tipo_rubro, collapse="|"), y[,2]),c(2,4:8)] %>% mutate(fecha = fecha, lugar = lugar) # extraer datos numericos
-          
-    return(y)
-  }) %>% rbindlist(use.names = TRUE) 
-  
-  colnames(n) = c("nombre", "presupuesto_inicial","presupuesto_definitivo","compromisos","total_obligaciones","PAGOS", "fecha", "lugar") 
-  n 
-}
-
-resultados = f_extrac(list_chip, tipo_rubro = c("TOTAL INVERSIÓN","DEPORTE Y RECREACIÓN"))
 
   
   
